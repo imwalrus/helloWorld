7746c632b4402efe951c3ae224cab85ade31c072
imwalrus <im_walrus@naver.com>
2020-11-30 14:20:39 +0900

    1

---
+++ helloWorld/src/helloWorld/HelloWorld.java
@@ -0,0 +1,19 @@
+package helloWorld;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		String name = "30";// "yun jinseob";
+		name = "이재일";
+		System.out.println("my name is " + name);
+		int age = 25;
+		System.out.println("my name is " + age);
+		double height = 177;
+		System.out.println(name + "의 키는 " + height + " 입니다 ");
+
+		String name1 = "윤진섭";
+		int age1 = 25;
+		double hegiht1 = 180.5;
+		System.out.println(name1 + "의 키는 " + hegiht1 + "이며 나이는 " + age1 + "입니다");
+		System.out.println("두사람 나이의 합은 " + (age + age1) + "입니다");
+	}
+}
